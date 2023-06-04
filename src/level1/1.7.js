93efba7efb7270d05db2190852996236c6a5d3a9
htrang1815 <htrang1815@gmail.com>
2023-06-04 08:42:00 +0700

    1.7

---
+++ src/level1/1.7.js
@@ -0,0 +1,12 @@
+/**
+ * Write a program that takes a list of strings as input and sorts the list in alphabetical order.
+ * @param {array} arr
+ * @returns sorts the list in alphabetical order
+ */
+
+function listAlphabeticalOrder(arr) {
+    arr.sort();
+    return arr; 
+}
+
+module.exports = listAlphabeticalOrder;
