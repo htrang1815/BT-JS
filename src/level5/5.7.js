ed0a5cbb645b0ef7d46740cd98ad5da4ec3e46ed
htrang1815 <htrang1815@gmail.com>
2023-06-04 09:26:51 +0700

    5.7

---
+++ src/level5/5.7.js
@@ -0,0 +1,20 @@
+/**
+MapKey: Cho 1 mảng các key, vào 1 mảng các object , Viết một function để trả ra một mảng các object theo thứ tự mảng các key. ( Yêu cầu dùng hàm map )
+
+ * @param {array} arr
+ * @param {array} key
+ * @returns MapKey
+ */
+function MapKey(arr, key) {
+  let result = [];
+  arr.map((x) => {
+    let cur = {};
+    for (let i = 0; i < arr.length; i++) {
+      cur[key[i]] = x[key[i]];
+    }
+    result.push(cur);
+  });
+  return result;
+}
+
+module.exports = MapKey;
